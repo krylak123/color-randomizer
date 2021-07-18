de827e5ccd9921561ffccdaab7d0c7ff539c9708
Krystian Grzelak <krystian.grzelak@o2.pl>
2021-07-18 15:12:10 +0200

    code formatted

--- script.js
+++ script.js
@@ -7,37 +7,37 @@ const titleLetters = [...document.querySelectorAll('.header__title span')];
 
 //funkcja losująca kolory w systemie szesnastkowym
 const colorRandom = () => {
-    const colors = `0123456789abcdef`;
-    let color = ``;
-    for (let i = 0; i < 6; i++) {
-        const number = Math.floor(Math.random() * colors.length);
-        color += colors[number];
-    }
-    return color;
-}
+  const colors = `0123456789abcdef`;
+  let color = ``;
+  for (let i = 0; i < 6; i++) {
+    const number = Math.floor(Math.random() * colors.length);
+    color += colors[number];
+  }
+  return color;
+};
 
 //pętla kolorująca litery h1
-titleLetters.forEach(letter => {
-    letter.style.color = `#${colorRandom()}`;
-    letter.style.textShadow = `0 0 2px #000`;
-})
+titleLetters.forEach((letter) => {
+  letter.style.color = `#${colorRandom()}`;
+  letter.style.textShadow = `0 0 2px #000`;
+});
 
 //pętla tworząca divy
 for (let i = 0; i < 15; i++) {
-    const divElement = document.createElement('div');
-    const color = colorRandom()
-    divElement.style.backgroundColor = `#${color}`;
-    divElement.textContent = `#${color}`
-    document.querySelector('.color-board').appendChild(divElement)
+  const divElement = document.createElement('divs');
+  const color = colorRandom();
+  divElement.style.backgroundColor = `#${color}`;
+  divElement.textContent = `#${color}`;
+  document.querySelector('.color-board').appendChild(divElement);
 }
 
 const divsColor = () => {
-    const divs = document.querySelectorAll('.color-board div')
-    divs.forEach(item => {
-        const color = colorRandom()
-        item.style.backgroundColor = `#${color}`;
-        item.textContent = `#${color}`
-    })
-}
+  const divs = document.querySelectorAll('.color-board div');
+  divs.forEach((item) => {
+    const color = colorRandom();
+    item.style.backgroundColor = `#${color}`;
+    item.textContent = `#${color}`;
+  });
+};
 
-document.querySelector('.header__btn').addEventListener('click', divsColor)
+document.querySelector('.header__btn').addEventListener('click', divsColor);
